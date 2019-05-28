a6c7462a59a148996a4361fd9037424127f25391
phivh <phivhnet@gmail.com>
2019-05-29 02:50:33 +0700

    Updated

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const query2 = 'some-query-2';
 const query3 = 'some-query-3';
 
 function getAccountId() {
-    sql.connect(connectionString).then(() => {
-        return sql.query(query1)
+    sql.connect(connectionString).then(cnn => {
+        return cnn.request().query(query1)
     }).then(result1 => {
         console.log(result1)
-        return sql.query(query2, {...result1})
+        return cnn.request().query(query2, {...result1})
     }).then(result2 => {
         console.log(result2)
-        return sql.query(query3, {...result2})
+        return cnn.request().query(query3, {...result2})
     }).then(result3 => {
         console.log(result3)
     }).catch(err => {
