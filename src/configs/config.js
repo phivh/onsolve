a8566327fe0a4da6b1f7b75c42b4f365a9d6d5c4
phivh <phivhnet@gmail.com>
2019-05-29 02:19:59 +0700

    Updated api key

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,6 +1,6 @@
 const marvelApi = {
-    publicKey: '13065ce22cdecaf8358b1b56dc54e2c7',
-    privateKey: 'c31db2a8d4c56f22f86dd437f66da424f96b8591',
+    publicKey: 'bc98ffc6709d037b1b147eec0015e3fb',
+    privateKey: 'da67b1ec2268d6360c1d43321265129f01361f18',
     baseUrl: `${window.location.protocol || 'http'}//gateway.marvel.com/v1/public/`
 }
 export default marvelApi;  
