1b3960ac0d870aec6dd81914337d291ad76a2200
phivh <phivhnet@gmail.com>
2019-05-30 10:36:06 +0700

    Updates

---
+++ precache-manifest.f845b1ce677c4f1e6248de50ba734240.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990bdee91088e4ff7b521b2150c0b80e",
+    "url": "/onsolve/index.html"
+  },
+  {
+    "revision": "7b6ac3687bcad9b2a6e4",
+    "url": "/onsolve/static/css/3.c2a69d58.chunk.css"
+  },
+  {
+    "revision": "85300a0ab507db49b084",
+    "url": "/onsolve/static/css/main.b1883070.chunk.css"
+  },
+  {
+    "revision": "7d5abebf3be7572507d4",
+    "url": "/onsolve/static/js/2.ec62ef53.chunk.js"
+  },
+  {
+    "revision": "7b6ac3687bcad9b2a6e4",
+    "url": "/onsolve/static/js/3.a0b06383.chunk.js"
+  },
+  {
+    "revision": "85300a0ab507db49b084",
+    "url": "/onsolve/static/js/main.3d622a66.chunk.js"
+  },
+  {
+    "revision": "e098fe743cb02680039c",
+    "url": "/onsolve/static/js/runtime~main.6b4cf232.js"
+  },
+  {
+    "revision": "e01a7139f544e5da838755bccc9aa2c5",
+    "url": "/onsolve/static/media/marvel-logo.e01a7139.jpg"
+  }
+]);
